e5ce7cb5a001c8f7bc604914e1e94d098017c824
Wataru NAKAI <watnk0806@gmail.com>
2020-10-22 23:26:22 +0900

    Replace sample code

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'whatalnk'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'whatalnk\'s Website'
 
 export default function Layout({ children, home }) {
     return (
@@ -13,13 +13,7 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.now.sh/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    content="This is a website I am building based on Next.js tutorial"
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a website I am building based on {' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -31,7 +30,6 @@ export default function Home({ allPostsData }) {
                 <Date dateString={date} />
               </small>
             </li>
-
           ))}
         </ul>
       </section>
